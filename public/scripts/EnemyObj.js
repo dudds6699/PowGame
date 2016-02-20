8a7ecbc212efd5c2ab6829405672d46a4ff1661c
Chris McBride <deviler151@gmail.com>
2016-02-20 14:28:43 -0600

    Added enemy that flies toward the player

---
+++ public/scripts/EnemyObj.js
@@ -0,0 +1,88 @@
+function enemyObj(){
+    this.texture = PIXI.Texture.fromImage('pics/enemy.png');
+    this.obj = new PIXI.Sprite(this.texture);
+    
+    //sprites anchor point
+    this.obj.anchor.x = 0.5;
+    this.obj.anchor.y = 0.5;
+    
+    //position
+    this.obj.position.x = 200;
+    this.obj.position.y = 150;
+    this.exists = true;
+    this.movey = false;
+    this.movex = false;
+    this.targetx = 0;
+    this.targety = 0;
+    this.v = 1;
+}
+
+function enemyObj(tex, anchorx, anchory, posx, posy){
+    this.texture =  PIXI.Texture.fromImage(tex);
+    this.obj = new PIXI.Sprite(this.texture);
+    
+    //sprites anchor point
+    this.obj.anchor.x = anchorx;
+    this.obj.anchor.y = anchory;
+    
+    //position
+    this.obj.position.x = posx;
+    this.obj.position.y = posy;
+    this.exists = true;
+    this.movey = false;
+    this.movex = false;
+    this.targetx = 0;
+    this.targety = 0;
+    this.v = 20;
+    
+    //this is a test of event handlers
+    this.obj.interactive = true;
+}
+
+enemyObj.prototype.setAnchor = function(x, y){
+    this.obj.anchor.x = x;
+    this.obj.anchor.y = y;
+};
+
+enemyObj.prototype.setPosition = function(x, y){
+    this.obj.position.x = x;
+    this.obj.position.y = y;
+};
+
+enemyObj.prototype.addSprite = function(container){
+    container.addChild(this.obj);
+};
+
+enemyObj.prototype.Move = function(x, y){
+    this.targetx = x;
+    this.targety = y;
+    this.movey = true;
+    this.movex = true;
+};
+
+enemyObj.prototype.Explode = function()
+{
+    this.visble = false;
+}
+
+enemyObj.prototype.Fly = function(targetx,targety)
+{
+    if(this.obj.position.x > targetx)
+    {
+        this.obj.position.x -= 1;
+    }
+    else if(this.obj.position.x < targetx){
+        this.obj.position.x += 1;
+    }
+    if(this.obj.position.y > targety)
+    {
+        this.obj.position.y -= 1;
+    }
+    else if(this.obj.position.y < targety){
+        this.obj.position.y += 1;
+    }
+    //if(this.obj.position.x >=  container.obj.width(integer))
+    //{
+        
+    //}
+}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -29,10 +29,13 @@ $(document).ready(function () {
         
         
     var check = 'pics/bunny.png';
+    var enemyimg = 'pics/enemy.png';
     var test = new playerObj(check, 0.5,0.5,400,300);
     test.addSprite(container);
     test.setPosition(100,500);
-    
+    var enemy = new enemyObj(enemyimg, 0.6,0.6,400,300);
+    enemy.addSprite(container);
+    enemy.setPosition(400,300);
     container.mousedown = function (event) 
     {
         test.Move(event.data.originalEvent.clientX, event.data.originalEvent.clientY);
@@ -41,6 +44,13 @@ $(document).ready(function () {
             
     function animate() {
         requestAnimationFrame( animate );
+        
+        if(enemy.exists === true)
+        {
+            enemy.Fly(test.obj.position.x, test.obj.position.y);
+            
+        }
+        
         //state();
         // just for fun, let's rotate mr rabbit a little
         if(test.exists === true){
