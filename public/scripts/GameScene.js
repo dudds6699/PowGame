c55667438218e564c9fafce5cb29e84c5f1f4fde
Chris Bodley <chris.matthew.bodley@gmail.com>
2016-02-20 17:24:15 -0600

    re worked so that the scense can be separated

---
+++ public/scripts/GameScene.js
@@ -0,0 +1,85 @@
+//var gameContainer = new PIXI.Container();
+
+function GameScene(x, y){
+    this.state = false;
+    this.maxX = x;
+    this.maxY = y;
+    
+    this.obj =  new PIXI.Container();
+    this.obj.interactive = true;
+    this.obj.visible = false;
+    this.obj.hitArea = new PIXI.Rectangle(0, 0, this.maxX, this.maxY);
+
+    // add the renderer view element to the DOM
+    //$('body').append(renderer.view);
+    
+    this.heroLocation = 'pics/Hero.png';
+    this.enemyimg = 'pics/Antagonist.png';
+    this.scoreDelay = 0;
+    this.maxDelay = 25;
+    this.score = 0;
+    
+    this.player = new playerObj(this.heroLocation, 0.5,0.5,100,100);
+    this.player.addSprite(this.obj);
+    this.player.setPosition(100,500);
+    
+    this.enemy = new enemyObj(this.enemyimg, 0.6,0.6,400,300);
+    this.enemy.addSprite(this.obj);
+    this.enemy.setPosition(400,300);
+    
+    this.score = new scoreObj(0.5,0.5, 700, 50);
+    this.score.addSprite(this.obj);
+    
+    this.SetHandlers();
+}
+
+GameScene.prototype.startGame = function() {
+    this.state = true;
+    this.obj.visible = true;
+};
+
+GameScene.prototype.SetHandlers = function(){
+    var pl = this.player;  
+    
+    this.obj.mousedown = function (event) 
+    {
+        pl.Move(event.data.originalEvent.clientX, event.data.originalEvent.clientY);
+        //playWoosh();
+    };
+    
+    this.obj.tap  = function(event){
+        pl.Move(event.data.global.x, event.data.global.y);
+        //playWoosh();
+    };  
+};
+
+GameScene.prototype.Animate = function () {
+       
+        if(this.state === true){
+            
+            if(this.enemy.exists === true)
+            {
+                if(this.enemy.Fly(this.player.obj.position.x, this.player.obj.position.y))
+                {
+                    //playGrunt();
+                }
+            
+            }
+        
+        //state();
+        // just for fun, let's rotate mr rabbit a little
+            if(this.player.exists === true){
+                this.player.Animate();
+            }else{
+                //this.state = false;
+            }
+        
+            if(this.score.exists === true && (this.scoreDelay == this.maxDelay)){
+                this.score.Animate();
+                this.scoreDelay = 0;
+            }else{
+                this.scoreDelay++;
+            }
+ 
+        }
+    };
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,93 +1,33 @@
-    var state = true;     
+var renderer;
 $(document).ready(function () {
     var resx = 800;
     var resy =  600;
-    var scoreDelay = 0;
-    var maxDelay = 25;
-        
-    // create an new instance of a pixi stage
-    var container = new PIXI.Container();
     var loader = PIXI.loader;
-             
-    // create a renderer instance.
-    var renderer = PIXI.autoDetectRenderer(resx, resy,{backgroundColor : 0x1099bb});
-    state = true;             
-    container.interactive = true;
+    var gameScene = new GameScene(resx, resy);
     
-    //this we need to keep
-    container.hitArea = new PIXI.Rectangle(0, 0, resx, resy);
-
-
-    // add the renderer view element to the DOM
-    //$('body').append(renderer.view);
-    document.getElementById("game").appendChild(renderer.view); 
-    
-    var heroLocation = 'pics/Hero.png';
+    renderer = PIXI.autoDetectRenderer(resx, resy,{backgroundColor : 0x1099bb});
     
     loader
-        .add(heroLocation)
-        .load(animate);   
-        
-        
-
+        .add('pics/Hero.png')
+        .add('pics/Antagonist.png')
+        .load(start);
     
-    var player = new playerObj(heroLocation, 0.5,0.5,100,100);
-    player.addSprite(container);
-    player.setPosition(100,500);
-
-    var enemyimg = 'pics/Antagonist.png';
-
-    var enemy = new enemyObj(enemyimg, 0.6,0.6,400,300);
-    enemy.addSprite(container);
-    enemy.setPosition(400,300);
     
-    //score card
-    var score = new scoreObj(0.5,0.5, 700, 50);
-    score.addSprite(container);
-
-    container.mousedown = function (event) 
-    {
-        player.Move(event.data.originalEvent.clientX, event.data.originalEvent.clientY);
-        playWoosh();
-    };
     
-    container.tap  = function(event){
-        player.Move(event.data.global.x, event.data.global.y);
-        playWoosh();
-    };
+    document.getElementById("game").appendChild(renderer.view); 
+   
     
-            
-    function animate() {
-        requestAnimationFrame( animate );
-        
-        if(enemy.exists === true)
-        {
-            if(enemy.Fly(player.obj.position.x, player.obj.position.y))
-            {
-                playGrunt();
-            }
-            
-        }
-        
-        //state();
-        // just for fun, let's rotate mr rabbit a little
-        if(player.exists === true){
-            player.Animate();
-        }else{
-            state = false;
-        }
-        
-        if(score.exists === true && (scoreDelay == maxDelay)){
-            score.Animate();
-            scoreDelay = 0;
-        }else{
-            scoreDelay++;
-        }
-  
-        if(state === true){
-            renderer.render(container);
-        }
+    function start(){
+        gameScene.startGame();
+        animate();
     }
+    
+    function animate(){
+         requestAnimationFrame( animate );
+         gameScene.Animate();
+         renderer.render(gameScene.obj);
+    }
+
     
     function playWoosh()
     {
