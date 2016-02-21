603775e7fa894b75f159210dd54c4d8e4a143534
Chris Bodley <chris.matthew.bodley@gmail.com>
2016-02-21 14:08:22 -0600

    shrunk the fire place and set it so the characters will always overlap

--- public/scripts/GameScene.js
+++ public/scripts/GameScene.js
@@ -25,7 +25,13 @@ function GameScene(x, y){
     this.background = new gameObj(this.backgroundimg, 0,0,0,0);
     this.background.addSprite(this.obj);
     
+    this.fire = new hazardObj(this.fireimg, 0.5,0.5);
+    this.fire.obj.scale.y = 0.5;
+    this.fire.obj.scale.x = 0.5;
     
+    
+    this.fire.addSprite(this.obj);
+
     this.player = new playerObj(this.heroLocation, 0.5,0.5,100,100);
     this.player.addSprite(this.obj);
     this.player.setPosition(100,500);
@@ -37,8 +43,7 @@ function GameScene(x, y){
     this.score = new scoreObj(0.5,0.5, 700, 50);
     this.score.addSprite(this.obj);
     
-    this.fire = new hazardObj(this.fireimg, 0.5,0.5);
-    this.fire.addSprite(this.obj);
+
     
     this.SetHandlers();
 }
